fa1bc11211a631153646628748fa1670ab551ede
ZALMEZ-PC\Cato Myhre <boredgamerz1998@gmail.com>
2016-08-21 23:11:26 +0200

    Added: + Account System     * Username and password     * Register a new account
    Currently working on:
     * Displaying health
     * A clip with a max amount bullet with reload time.
     * Debugging in general

---
+++ Assets/Scripts/CursorState.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CursorState : NetworkBehaviour
+{
+
+    public static bool isCursorLocked = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            //CheckForInput();
+            CheckIfCursorShouldBeLocked();
+        }
+    }
+
+    void ToogleCursorState()
+    {
+        if (PauseMenu.IsOn)
+        {
+            isCursorLocked = false;
+        }
+        else
+        {
+            isCursorLocked = true;
+        }
+    }
+
+    void CheckForInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToogleCursorState();
+        }
+    }
+
+    void CheckIfCursorShouldBeLocked()
+    {
+        if (PauseMenu.IsOn)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (!PauseMenu.IsOn)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
+
