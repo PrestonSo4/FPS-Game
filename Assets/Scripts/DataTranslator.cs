f54456d4d2111a52933c7fe1ae5981d26ff0fa91
ZALMEZ-PC\Cato Myhre <boredgamerz1998@gmail.com>
2016-10-07 08:37:11 +0200

    *Changelog:*  * Connected account system to the game  * Added Death And Kill Counter
    *Bug Fixes*
     * "Did someone say god like?" Fixed an issue that  made players immortal
    
    *Known bugs*
     * "UP UP AND AWAY! :D", there's a bug that can make you fly up in the air by holding space and press escape once, then release all keys aaaaannnd... wow! that player can fly!
     * "Ammo? what's that?", there's a bug that can make you fire without stop(cannot be controlled) it's basically the same bug as the one above, but you hold your fire button!

---
+++ Assets/Scripts/DataTranslator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System;
+
+public class DataTranslator : MonoBehaviour {
+
+    private static string KILL_SYMBOL = "[KILLS]";
+    private static string DEATH_SYMBOL = "[DEATHS]";
+
+    public static string ValuesToData (int kills, int deaths)
+    {
+        return KILL_SYMBOL + kills + "/"+ DEATH_SYMBOL + deaths;
+    }
+
+    public static int DataToKills (string data)
+    {
+        return int.Parse (DataToValue(data, KILL_SYMBOL));
+    }
+
+    public static int DataToDeaths(string data)
+    {
+        return int.Parse (DataToValue(data, DEATH_SYMBOL));
+    }
+
+    private static string DataToValue(string data, string symbol)
+    {
+        string[] pieces = data.Split('/');
+
+        foreach (string piece in pieces)
+        {
+            if (piece.StartsWith(symbol))
+            {
+               return piece.Substring(symbol.Length);
+            }
+        }
+        Debug.LogError(symbol + " not found in" + data);
+        return "";
+    }
+
+}
---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScore : MonoBehaviour {
+
+    Player player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GetComponent<Player>();
+        StartCoroutine(SyncScoreLoop());
+	}
+
+    void SyncNow()
+    {
+        if (UserAccountManager.IsLoggedIn)
+        {
+            UserAccountManager.instance.GetData(OnDataRecived);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if(player != null)
+            SyncNow();
+    }
+
+    IEnumerator SyncScoreLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            SyncNow();
+        }
+    }
+
+
+
+    void OnDataRecived(string data)
+    {
+        if (player.kills == 0 && player.deaths == 0)
+            return;
+
+        int kills = DataTranslator.DataToKills(data);
+        int deaths = DataTranslator.DataToDeaths(data);
+
+        int newKills = player.kills + kills;
+        int newDeaths = player.deaths + deaths;
+
+        string newData = DataTranslator.ValuesToData(newKills, newDeaths);
+
+        Debug.Log("Syncing: " + newData);
+
+        player.kills = 0;
+        player.deaths = 0;
+
+        UserAccountManager.instance.SendData(newData);
+    }	
+}
