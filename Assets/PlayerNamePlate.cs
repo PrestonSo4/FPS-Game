bebdc29a734cc010e4c8209f34c1fd721f442f9d
ZALMEZ-PC\Cato Myhre <boredgamerz1998@gmail.com>
2016-12-07 20:54:50 +0100

    fixed some bugs

---
+++ Assets/PlayerNamePlate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNamePlate : MonoBehaviour {
+
+    [SerializeField]
+    private Text usernameText;
+
+    [SerializeField]
+    private Player player;
+
+    [SerializeField]
+    private RectTransform healthBarFill;
+
+	void Update()
+    {
+        usernameText.text = player.username;
+        healthBarFill.localScale = new Vector3(player.GetHealthPct(), 1f, 1f);
+
+    }
+
+
+}
